2ff9f513fe7e363cba66d50a9cace9f3aaee1949
daredevil0005 <pratikdabhane5002@gmail.com>
2026-08-02 22:18:08 +0530

    Making changes in car game

--- main/car.js
+++ main/car.js
@@ -1179,3 +1179,107 @@ function updateHighscore() {
 
 }
 
+// ==========================================================
+// Initialize Game
+// ==========================================================
+
+function init() {
+
+    game.style.width = width + "px";
+    game.style.height = height + "px";
+
+    hero.style.top = height - 80 + "px";
+    hero.style.left =
+        halfWidth - ASSETS.IMAGE.HERO.width / 2 + "px";
+
+    hero.style.background =
+        `url(${ASSETS.IMAGE.HERO.src})`;
+
+    hero.style.width =
+        ASSETS.IMAGE.HERO.width + "px";
+
+    hero.style.height =
+        ASSETS.IMAGE.HERO.height + "px";
+
+    cloud.style.backgroundImage =
+        `url(${ASSETS.IMAGE.SKY.src})`;
+
+    audio = new AudioManager();
+
+    Object.keys(ASSETS.AUDIO).forEach(key => {
+
+        audio.load(
+
+            ASSETS.AUDIO[key],
+
+            key,
+
+            () => {}
+
+        );
+
+    });
+
+    cars.push(new Car(0, ASSETS.IMAGE.CAR, LANE.C));
+    cars.push(new Car(10, ASSETS.IMAGE.CAR, LANE.B));
+    cars.push(new Car(20, ASSETS.IMAGE.CAR, LANE.C));
+    cars.push(new Car(35, ASSETS.IMAGE.CAR, LANE.C));
+    cars.push(new Car(50, ASSETS.IMAGE.CAR, LANE.A));
+    cars.push(new Car(60, ASSETS.IMAGE.CAR, LANE.B));
+    cars.push(new Car(70, ASSETS.IMAGE.CAR, LANE.A));
+
+    for (let i = 0; i < N; i++) {
+
+        const line = new Line();
+
+        line.z = i * segL + 270;
+
+        for (let j = 0; j < 8; j++) {
+
+            const element = document.createElement("div");
+
+            road.appendChild(element);
+
+            line.elements.push(element);
+
+        }
+
+        lines.push(line);
+
+    }
+
+    for (let i = 0; i < 12; i++) {
+
+        const element = document.createElement("p");
+
+        highscore.appendChild(element);
+
+    }
+
+    updateHighscore();
+
+    reset();
+
+}
+
+// ==========================================================
+// Main Loop
+// ==========================================================
+
+(function gameLoop() {
+
+    requestAnimationFrame(gameLoop);
+
+    const now = timestamp();
+
+    const delta = now - then;
+
+    if (delta > targetFrameRate) {
+
+        then = now - (delta % targetFrameRate);
+
+        update(delta / 1000);
+
+    }
+
+})();
