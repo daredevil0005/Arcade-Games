62a147fbfbb901ee9f6bb02bd8976bd1df77e715
daredevil0005 <pratikdabhane5002@gmail.com>
2026-08-02 21:57:02 +0530

    Making changes in car game

--- main/car.js
+++ main/car.js
@@ -188,3 +188,80 @@ function updateKey(event) {
 window.addEventListener("keydown", updateKey);
 window.addEventListener("keyup", updateKey);
 
+// ==========================================================
+// Line Class
+// ==========================================================
+
+class Line {
+
+    constructor() {
+
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+
+        this.X = 0;
+        this.Y = 0;
+        this.W = 0;
+
+        this.curve = 0;
+        this.scale = 0;
+
+        this.elements = [];
+        this.special = null;
+    }
+
+    project(camX, camY, camZ) {
+
+        this.scale = camD / (this.z - camZ);
+
+        this.X = (1 + this.scale * (this.x - camX)) * halfWidth;
+
+        this.Y = Math.ceil(
+            ((1 - this.scale * (this.y - camY)) * height) / 2
+        );
+
+        this.W = this.scale * roadW * halfWidth;
+    }
+
+    clearSprites() {
+
+        for (const element of this.elements) {
+
+            element.style.background = "transparent";
+
+        }
+
+    }
+
+    drawSprite(depth, layer, sprite, offset) {
+
+        let destX = this.X + this.scale * halfWidth * offset;
+        let destY = this.Y + 4;
+
+        let destW = (sprite.width * this.W) / 265;
+        let destH = (sprite.height * this.W) / 265;
+
+        destX += destW * offset;
+        destY -= destH;
+
+        const object =
+            layer instanceof Element
+                ? layer
+                : this.elements[layer + 6];
+
+        object.style.background =
+            `url('${sprite.src}') no-repeat`;
+
+        object.style.backgroundSize =
+            `${destW}px ${destH}px`;
+
+        object.style.left = destX + "px";
+        object.style.top = destY + "px";
+        object.style.width = destW + "px";
+        object.style.height = destH + "px";
+        object.style.zIndex = depth;
+    }
+
+}
+
